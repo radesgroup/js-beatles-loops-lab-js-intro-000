ea6ba8d85ed5dc6ad853f02e1c5045e87b2385dd
Aleksey <fduchunru@gmail.com>
2020-10-02 14:05:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,38 +21,47 @@ function johnLennonFacts(facts) {
   return newFacts
 }
 
+function iLoveTheBeatles(n) {
+  const arr = []
 
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
 
 var r = 7;
 var loveBeatlesEmpty = [];
-function iLoveTheBeatles(r){
-let dif = 0;
-  if (r < 15){
-    do {
-    loveBeatlesEmpty.push("I love the Beatles!")
-    dif++;
-       }
-    while (dif < (15 - r));
-    return loveBeatlesEmpty;
-             }
-  else if(r >= 15){
-    loveBeatlesEmpty = ["I love the Beatles!"];
-  }return loveBeatlesEmpty;
-}
+//function iLoveTheBeatles(r){
+//let dif = 0;
+//  if (r < 15){
+  //  do {
+    //loveBeatlesEmpty.push("I love the Beatles!")
+    //dif++;
+      // }
+  //  while (dif < (15 - r));
+    //return loveBeatlesEmpty;
+  //           }
+//  else if(r >= 15){
+    //loveBeatlesEmpty = ["I love the Beatles!"];
+//  }return loveBeatlesEmpty;
+//}
 
-var r = 17;
-var loveBeatlesEmpty = [];
-function iLoveTheBeatles(r){
-let dif = 0;
-  if (r < 15){
-    do {
-    loveBeatlesEmpty.push("I love the Beatles!")
-    dif++;
-       }
-    while (dif < (15 - r));
-    return loveBeatlesEmpty;
-             }
-  else if(r >= 15){
-    loveBeatlesEmpty = ["I love the Beatles!"];
-  }return loveBeatlesEmpty;
-}
+//var r = 17;
+//var loveBeatlesEmpty = [];
+//function iLoveTheBeatles(r){
+//let dif = 0;
+//  if (r < 15){
+  //  do {
+    //loveBeatlesEmpty.push("I love the Beatles!")
+    //dif++;
+      // }
+    //while (dif < (15 - r));
+    //return loveBeatlesEmpty;
+      //       }
+  //else if(r >= 15){
+    //loveBeatlesEmpty = ["I love the Beatles!"];
+  //}return loveBeatlesEmpty;
+//}
